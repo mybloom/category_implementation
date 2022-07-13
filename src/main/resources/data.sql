5d11a5f577646f92bc5bcd652db4443dc96ed143
mybloom <baejungeun11@gmail.com>
2022-07-13 15:27:06 +0900

    feat: 카테고리 샘플 데이터용 data.sql 생성 (#4)
    - 테이블 생성 이후 data.sql 실행되도록 환경 셋팅
    defer-datasource-initialization: true

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into category (title) values ('아우터');
+insert into category (title, parent_category_id) values ('쟈켓',1);
+insert into category (title, parent_category_id) values ('가디건',1);
+insert into category (title, parent_category_id) values ('블루종',1);
