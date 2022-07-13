9daa178f2b03eae79743040f8b2778899030f696
mybloom <baejungeun11@gmail.com>
2022-07-13 16:23:39 +0900

    feat: 테스트코드용 환경설정 (#4)
    - 테스트용 appliction.yml, data-test.sql, logback 설정

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,4 @@
+insert into category (title) values ('아우터_테스트');
+insert into category (title, parent_category_id) values ('쟈켓_테스트',1);
+insert into category (title, parent_category_id) values ('가디건_테스트',1);
+insert into category (title, parent_category_id) values ('블루종_테스트',1);
