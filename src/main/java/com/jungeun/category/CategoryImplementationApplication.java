0d8a1834a20553666011df32658bb16afe9b44d5
mybloom <baejungeun11@gmail.com>
2022-07-13 13:17:29 +0900

    feat: project 초기셋팅

---
+++ src/main/java/com/jungeun/category/CategoryImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.jungeun.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jungeun/category/CategoryImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungeun.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
