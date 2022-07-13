e86cdbaee7e8ad07dc891708a808376af218573f
mybloom <baejungeun11@gmail.com>
2022-07-13 17:26:03 +0900

    feat: Category 엔티티에 getter생성 (#4)
    - 엔티티 -> dto로 변환로직을 dto에 작성하여 엔티티getter 사용

--- src/main/java/com/jungeun/category/domain/Category.java
+++ src/main/java/com/jungeun/category/domain/Category.java
@@ -11,8 +11,10 @@ import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Builder(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
