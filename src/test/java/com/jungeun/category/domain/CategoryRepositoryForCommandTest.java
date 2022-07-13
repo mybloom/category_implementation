bc9b0552a283205858cf277e5a1623bd35f2b432
mybloom <baejungeun11@gmail.com>
2022-07-13 23:27:09 +0900

    feat: 상위카테고리 repository 등록 테스트 메서드 작성 (#5)

---
+++ src/test/java/com/jungeun/category/domain/CategoryRepositoryForCommandTest.java
@@ -0,0 +1,34 @@
+package com.jungeun.category.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.jungeun.category.util.EntityCreator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DisplayName("Category Repository For Command Test")
+@DataJpaTest
+public class CategoryRepositoryForCommandTest {
+
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@DisplayName("카테고리 등록 테스트")
+	@Test
+	void save() {
+		//given
+		Category category = EntityCreator.상위카테고리_생성_요청(categoryRepository);
+
+		//when
+		Category savedCategory = categoryRepository.save(category);
+
+		//then
+		assertAll(
+			() -> assertThat(savedCategory.getId()).isNotNull(),
+			() -> assertThat(savedCategory.getTitle()).isEqualTo(category.getTitle())
+		);
+	}
+}
