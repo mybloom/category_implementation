d076f8ad0a5c02409909b7ec036655820a12d233
mybloom <baejungeun11@gmail.com>
2022-07-15 18:27:12 +0900

    refactor: 상위카테고리 이용해 하위카테고리 가져오는 쿼리 수정 (#9)
    - 기존에는 하위카테고리 정보만 가져왔으나 상위카테고리 정보까지 조인해서 함께 가져오는 것으로 변경.
    - select projection이 동일 테이블 조인이므로 튜플 인터페이스를 이용해 조회 결과 매칭.

--- src/main/java/com/jungeun/category/domain/CategoryRepository.java
+++ src/main/java/com/jungeun/category/domain/CategoryRepository.java
@@ -1,9 +1,25 @@
 package com.jungeun.category.domain;
 
+import com.jungeun.category.controller.dto.ICategoryJoin;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-	List<Category> findByParent(Category parent);
+	@Query(
+		"select distinct "
+			+ "p.id as superCategoryId, "
+			+ "p.title as superTitle, "
+			+ "p.parent.id as superParentCategoryId, "
+			+ "s.id as categoryId, "
+			+ "s.title as title, "
+			+ "s.parent.id as parentCategoryId "
+			+ "from Category p "
+			+ "left join Category s "
+			+ "on p.id = s.parent.id "
+			+ "where s.parent.id = :parentCategoryId ")
+	List<ICategoryJoin> findByParentCategoryId(@Param("parentCategoryId") Long parentCategoryId);
+
 }
