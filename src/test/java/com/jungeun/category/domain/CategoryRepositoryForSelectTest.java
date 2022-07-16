811490f6ea9454e110088a95bb63f9236ccb2a6e
mybloom <baejungeun11@gmail.com>
2022-07-16 22:43:28 +0900

    test.fix: 상위카테고리를 이용해 하위 모든 카테고리 조회 성공용 repository 메서드 수정 (#10)

--- src/test/java/com/jungeun/category/domain/CategoryRepositoryForSelectTest.java
+++ src/test/java/com/jungeun/category/domain/CategoryRepositoryForSelectTest.java
@@ -3,13 +3,13 @@ package com.jungeun.category.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.jungeun.category.controller.dto.ICategoryJoin;
-import java.util.List;
+import com.jungeun.category.exception.CategoryNoDataFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DisplayName("CategoryRepository 조회 메서드 테스트")
 @DataJpaTest
 class CategoryRepositoryForSelectTest {
 
@@ -17,20 +17,20 @@ class CategoryRepositoryForSelectTest {
 	CategoryRepository categoryRepository;
 
 	final Long SUPER_CATEGORY_ID = 1L;
+	final Long SUB_CATEGORY_ID = 2L;
+
 
 	@DisplayName("상위카테고리를 이용해 하위 모든 카테고리 조회")
 	@Test
 	void findById() {
-		//given
-		Category category = Category.of(SUPER_CATEGORY_ID);
-
 		//when
-		List<ICategoryJoin> findCategories = categoryRepository.findByParentCategoryId(SUPER_CATEGORY_ID);
+		Category findCategory = categoryRepository.findById(SUPER_CATEGORY_ID)
+			.orElseThrow(CategoryNoDataFoundException::new);
 
 		//then
 		assertAll(
-			() -> assertThat(findCategories).isNotNull(),
-			() -> assertThat(findCategories).hasSize(3)
+			() -> assertThat(findCategory).isNotNull(),
+			() -> assertThat(findCategory.getSubCategories()).hasSize(3)
 		);
 	}
 
