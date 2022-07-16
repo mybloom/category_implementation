b7ea878a05d6a2d1affed547e0ea314b49a2aa91
mybloom <baejungeun11@gmail.com>
2022-07-16 22:35:47 +0900

    refactor: repository에 불필요한 메서드 삭제 (#10)
    - jpql로 작성한 메서드, 쿼리메서드로 대체 가능하여 삭제

--- src/main/java/com/jungeun/category/domain/CategoryRepository.java
+++ src/main/java/com/jungeun/category/domain/CategoryRepository.java
@@ -1,28 +1,10 @@
 package com.jungeun.category.domain;
 
-import com.jungeun.category.controller.dto.ICategoryJoin;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-	@Query(
-		"select distinct "
-			+ "p.id as superCategoryId, "
-			+ "p.title as superTitle, "
-			+ "p.parent.id as superParentCategoryId, "
-			+ "s.id as categoryId, "
-			+ "s.title as title, "
-			+ "s.parent.id as parentCategoryId "
-			+ "from Category p "
-			+ "left join Category s "
-			+ "on p.id = s.parent.id "
-			+ "where s.parent.id = :parentCategoryId ")
-	List<ICategoryJoin> findByParentCategoryId(@Param("parentCategoryId") Long parentCategoryId);
 
 	@EntityGraph(attributePaths = {"subCategories"})
 	@Override
