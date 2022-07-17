698fe28aa6495856276a4a74762a4d2922eca2c3
mybloom <baejungeun11@gmail.com>
2022-07-17 15:05:09 +0900

    data: 카테고리 샘플데이터에 category_order 추가 (#12)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,16 @@
 --category depth1
-insert into category (title) values ('아우터');
-insert into category (title) values ('상의');
-insert into category (title) values ('바지');
+insert into category (title, category_order) values ('아우터', 1);
+insert into category (title, category_order) values ('상의', 3);
+insert into category (title, category_order) values ('바지', 2);
 
 --category depth2
-insert into category (title, parent_category_id) values ('쟈켓',1);
-insert into category (title, parent_category_id) values ('가디건',1);
-insert into category (title, parent_category_id) values ('블루종',1);
+insert into category (title, parent_category_id, category_order) values ('쟈켓', 1, 3);
+insert into category (title, parent_category_id, category_order) values ('가디건', 1, 1);
+insert into category (title, parent_category_id, category_order) values ('블루종', 1, 2);
 
-insert into category (title, parent_category_id) values ('긴팔',2);
-insert into category (title, parent_category_id) values ('반팔',2);
+insert into category (title, parent_category_id, category_order) values ('긴팔', 2, 2);
+insert into category (title, parent_category_id, category_order) values ('반팔', 2, 1);
 
-insert into category (title, parent_category_id) values ('긴바지',3);
-insert into category (title, parent_category_id) values ('반바지',3);
+insert into category (title, parent_category_id, category_order) values ('긴바지', 3, 3);
+insert into category (title, parent_category_id, category_order) values ('미디바지', 3, 2);
+insert into category (title, parent_category_id, category_order) values ('반바지', 3, 1);
