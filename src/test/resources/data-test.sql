570020c4ca666091582201c2f1a07f32c9fc8d88
mybloom <baejungeun11@gmail.com>
2022-07-17 16:34:05 +0900

    test: 테스트용 샘플 데이터 추가 (#12)

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,4 +1,10 @@
-insert into category (title) values ('아우터_테스트');
-insert into category (title, parent_category_id) values ('쟈켓_테스트',1);
-insert into category (title, parent_category_id) values ('가디건_테스트',1);
-insert into category (title, parent_category_id) values ('블루종_테스트',1);
+--depth1
+insert into category (title, category_order) values ('아우터_테스트', 1);
+insert into category (title, category_order) values ('상의_테스트', 2);
+
+--depth2
+insert into category (title, parent_category_id, category_order) values ('쟈켓_테스트',1, 2);
+insert into category (title, parent_category_id, category_order) values ('가디건_테스트',1, 3);
+insert into category (title, parent_category_id, category_order) values ('블루종_테스트',1, 1);
+
+insert into category (title, parent_category_id, category_order) values ('반팔_테스트',2, 1);
